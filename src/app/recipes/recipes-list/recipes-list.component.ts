01cb2a2fed762681db59c9e29901239abb0b254e
Bien Jerico <bien.jerico@gmail.com>
2018-02-07 16:43:49 +0400

    first commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe'
+              ,'This is a simple a test'
+              ,'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
+              new Recipe('A Test Recipe'
+              ,'This is a simple a test'
+              ,'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+  ];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
