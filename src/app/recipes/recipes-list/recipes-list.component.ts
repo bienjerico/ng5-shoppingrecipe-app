617587094637e1d91034622b1bde10a96463ebc4
Bien Jerico <bien.jerico@gmail.com>
2018-02-11 14:21:59 +0400

    apply directive menu isOpen

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,18 +8,24 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe'
               ,'This is a simple a test'
               ,'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-              new Recipe('A Test Recipe'
-              ,'This is a simple a test'
+    new Recipe('B Recipe'
+              ,'This is a simple a test B'
               ,'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
   ];
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
